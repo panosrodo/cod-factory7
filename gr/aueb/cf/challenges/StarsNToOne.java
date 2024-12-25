2a3d3262f0a5cd11c3d5fac19ca84fd8b2c891e2
panosrodo <brandonroots@hotmail.gr>
2024-12-25 14:59:43 +0200

    initial commit

---
+++ gr/aueb/cf/challenges/StarsNToOne.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.challenges;
+
+import java.util.Scanner;
+
+public class StarsNToOne {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStars = 0;
+
+        System.out.println("Please insert the number of stars");
+        numberOfStars = scanner.nextInt();
+
+        // Descending
+        System.out.println("Descending Stars");
+        for (int i = numberOfStars; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
