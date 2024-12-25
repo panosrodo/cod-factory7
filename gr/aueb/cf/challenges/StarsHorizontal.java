840a5f39e5a64879632168cce7f9d276209a977e
panosrodo <brandonroots@hotmail.gr>
2024-12-25 13:38:10 +0200

    initial commit

---
+++ gr/aueb/cf/challenges/StarsHorizontal.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.challenges;
+
+import java.util.Scanner;
+
+public class StarsHorizontal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStars = 0;
+
+        System.out.println("Please insert the number of stars");
+        numberOfStars = scanner.nextInt();
+
+        // Horizontal Stars
+        System.out.println("Horizontal Stars");
+        for (int i = 0; i < numberOfStars; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+}
