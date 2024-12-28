6aa6c214fd5281b89a213bde57c0bd11ff224bb8
panosrodo <brandonroots@hotmail.gr>
2024-12-28 16:41:47 +0200

    initial commit

---
+++ gr/aueb/cf/study/FacePrinter.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.study;
+
+public class FacePrinter {
+    public static void main(String[] args) {
+        // Display a pattern to create an ASCII art representation of a simple face
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
