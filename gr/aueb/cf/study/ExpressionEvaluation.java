7140b0972b9ac321f8dfce288cba34d809656779
panosrodo <brandonroots@hotmail.gr>
2024-12-28 11:43:46 +0200

    initial commit

---
+++ gr/aueb/cf/study/ExpressionEvaluation.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.study;
+
+public class ExpressionEvaluation {
+
+    public static void main(String[] args) {
+        // Calculate and print the result of a mathematical expression
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
