e0637440b11651e266756e76c02c15d1506b95f9
panosrodo <brandonroots@hotmail.gr>
2024-12-28 11:46:38 +0200

    initial commit

---
+++ gr/aueb/cf/study/FormulaComputation.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.study;
+
+public class FormulaComputation {
+
+    public static void main(String[] args) {
+        // Calculate the result of an alternating series and store it in 'result'
+        double result = 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
+
+        // Print the calculated result
+        System.out.println(result);
+    }
+}
