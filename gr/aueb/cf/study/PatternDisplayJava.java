e8539e138abf48b93c5c998fdc272b891f0f17b7
panosrodo <brandonroots@hotmail.gr>
2024-12-28 11:35:02 +0200

    initial commit

---
+++ gr/aueb/cf/study/PatternDisplayJava.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.study;
+
+public class PatternDisplayJava {
+
+    public static void main(String[] args) {
+        // Display the characters to form the text "Java" in a specific pattern
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+
+}
