f2165d16a764442cf2fa08a937777faeea7171bb
panosrodo <brandonroots@hotmail.gr>
2025-05-13 20:22:39 +0300

    Refactor Rename

--- gr/aueb/cf/ch9/File4PdfBufferedCopyApp8.java
+++ gr/aueb/cf/ch9/File4PdfBufferedCopyApp8.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class PdfBufferedCopyApp {
+public class File4PdfBufferedCopyApp8 {
 
     public static void main(String[] args) {
         int b;
@@ -16,15 +16,13 @@ public class PdfBufferedCopyApp {
 
         try (FileInputStream fis = new FileInputStream("C:/tmp/dummy.pdf");
              FileOutputStream fos = new FileOutputStream("C:/tmp/dummy-out.pdf")) {
-
             start = System.currentTimeMillis();
             while ((b = fis.read(buffer)) != -1) {
                 fos.write(buffer, 0, b);
-                counter++;
+                counter += b;
             }
             end = System.currentTimeMillis();
             elapsedTime = (end - start) / 1000.0;
-
             System.out.printf("Το αρχείο με μέγεθος %.1fKB (%d bytes) αντιγράφηκε επιτυχώς\n",
                     (counter / 1024.0), counter);
             System.out.println("Elapsed Time: " + elapsedTime + " seconds");
